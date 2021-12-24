84e83fcf7d3e60debfb69cd84cfb4e4c641c2cc5
Darrin <ogrotten@monsto.com>
2021-12-24 10:24:35 -0600

    .

---
+++ src/grid.ts
@@ -0,0 +1,5 @@
+import Phaser from "phaser"
+
+export class GridScene extends Phaser.Scene {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { FirstGameScene } from './first-scene';
+import { GridScene } from './grid';
 import Phaser from 'phaser';
 import './style.css';
 
 const config: Phaser.Types.Core.GameConfig = {
-    width: 800,
-    height: 600,
-    type: Phaser.AUTO,
-    parent: 'game-container',
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 300 },
-        debug: false
-      }
-    },
-    scene: [FirstGameScene]
-  };
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    }
+  },
+  scene: [GridScene]
+};
 
 new Phaser.Game(config);
