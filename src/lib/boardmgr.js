b5830079b3e414fccc5303a00e685993ab96179c
Darrin <ogrotten@monsto.com>
2021-12-26 16:41:29 -0600

    hm logical group

--- src/lib/boardmgr.js
+++ src/lib/boardmgr.js
@@ -5,16 +5,18 @@ export class BoardManager extends Phaser.GameObjects.Group {
 	constructor(width, height) {
 		super({ key: "Manage Grid", active: true })
 
+		this.inventory = new Map()
 
 		this.cellx = width
 		this.celly = height
 	}
+
 	placeAt(xx, yy, obj) {
-		var x2 = this.cellx * xx + this.cellx / 2
-		var y2 = this.celly * yy + this.celly / 2
-		obj.x = x2
-		obj.y = y2
+		const xpixel = this.cellx * xx + this.cellx / 2
+		const ypixel = this.celly * yy + this.celly / 2
+		obj.x = xpixel
+		obj.y = ypixel
+		this.inventory.set([xx, yy], { group: "yo" })
+		console.log(`conlog: this.inventory`, this.inventory)
 	}
-
 }
-
