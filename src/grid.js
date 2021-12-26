1cd827dddda000e0278cdf8a2bcaac727869c03d
Darrin <ogrotten@monsto.com>
2021-12-26 08:24:41 -0600

    filling grid

--- src/grid.js
+++ src/grid.js
@@ -5,11 +5,22 @@ export class GridScene extends Phaser.Scene {
 		super({ key: "grid", active: true })
 	}
 	preload() {
+		this.height = this.game.config.height
+		this.width = this.game.config.width
 		this.objects = {};
-		const { width, height } = this.game.config
 	}
 	create() {
-		// this.add.grid(300, 300, 512, 256, 64, 64, 0xffff00).setAltFillStyle(0x88ff00).setOutlineStyle();
+		this.add.grid(
+			this.width / 2,
+			this.height / 2,
+			this.width,
+			this.height,
+			50,
+			50,
+			0x111111
+		)
+			.setAltFillStyle(0x181818)
+			.setOutlineStyle();
 		// this.add.grid(300, 300, 512, 256, 64, 64, 0xffff00).setAltFillStyle(0x88ff00).setOutlineStyle();
 	}
 }
