4908d07c1e49476ddbf7db33fd14f7ef8bf5169d
Darrin <ogrotten@monsto.com>
2021-12-27 18:47:29 -0600

    .

--- src/setup.js
+++ src/setup.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import { Board, HexagonGrid, QuadGrid } from 'phaser3-rex-plugins/plugins/board-components.js';
 
 export class Setup extends Phaser.Scene {
 	constructor() {
@@ -11,11 +12,64 @@ export class Setup extends Phaser.Scene {
 		this.game.config.setup = {
 			cols: 6,
 			rows: 12,
+			cell: 50,
 			colors: {
 				main: 0x111111,
 				alt: 0x141414,
 				lines: 0x181818,
 			}
 		}
+
 	}
+	create() {
+		const {
+			setup: {
+				colors: {
+					main, alt, lines
+				},
+				cols, rows, cell
+			},
+			width, height,
+		} = this.game.config
+
+		let board = new Board(this, {
+			grid: quadGrid(this, cols, rows, cell),
+			width: 6,
+			height: 12
+		})
+		debugger
+		board.forEachTileXY((tileXY, board) => {
+			let points = board.getGridPoints(tileXY.x, tileXY.y, true);
+			// debugger
+			// console.log(`conlog: inside foreach`, points)
+			this.add.graphics({
+				lineStyle: {
+					width: 5,
+					color: 0xff0000,
+					alpha: 1
+				},
+				strokePoints: { points, closeShape: true }
+			})
+			// gridGraphics.strokePoints(points, true);
+		}, this);
+	}
+	update() {
+		this.scene.setVisible(true)
+	}
+}
+
+const quadGrid = (scene, cols, rows, cell) => {
+	console.log(`conlog: cols, rows, cell`, cols, rows, cell)
+	let grid = new QuadGrid(scene, {
+		x: 0,
+		y: 0,
+		cellWidth: 50,
+		cellHeight: 50,
+		type: 0,
+		dir: 4,
+		// height: rows * cell,
+		// widght: cols * cell
+	});
+	// debugger
+	return grid;
 }
